a1d0a9ebd392c796bd1b977d408420f9d8ea5cc2
sivasrinivas <sivasrinivas.amara@gmail.com>
2016-08-14 00:03:35 -0700

    adding godep files

--- main.go
+++ main.go
@@ -8,12 +8,14 @@ import (
 )
 
 func handle(w http.ResponseWriter, r *http.Request) {
+	log.Println("request recived, responding message...")
 	fmt.Fprint(w, "hi, demo started")
 }
 
 func main() {
 	port := os.Getenv("PORT")
-	log.Println("port:",port)
+	log.Println("Port:",port)
 	http.HandleFunc("/", handle)
+	log.Println("Starting service...")
 	http.ListenAndServe(":"+port+"/", nil)
 }
