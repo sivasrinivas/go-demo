ccb4503f7add463a7076db338a304057d9f94631
sivasrinivas <sivasrinivas.amara@gmail.com>
2016-09-05 12:34:54 -0700

    Adding image data.json file

--- main.go
+++ main.go
@@ -3,19 +3,46 @@ package main
 import (
 	"net/http"
 	"fmt"
-	"log"
 	"github.com/gorilla/mux"
+	"io/ioutil"
+	"encoding/json"
+	"log"
 	"os"
 )
 
-func handle(w http.ResponseWriter, r *http.Request) {
-	log.Println("request recived, responding message...")
-	fmt.Fprint(w, "hi, demo started")
+var images Images
+
+type Images struct {
+	Images []Image `json:"images"`
+}
+
+type Image struct {
+	Id string `json:"id"`
+	Text string `json:"text"`
+	Description string `json:"description"`
+	ImageURL string `json:"image_url"`
+}
+
+func imagesHandle(w http.ResponseWriter, r *http.Request) {
+	bytes, err := json.MarshalIndent(images, "", " ")
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Fprint(w, string(bytes))
+}
+
+func init() {
+	dataFile, err := ioutil.ReadFile("./data.json")
+	if err != nil {
+		panic(err)
+	}
+	json.Unmarshal(dataFile, &images)
 }
 
 func main() {
 	port := os.Getenv("PORT")
 	router := mux.NewRouter()
+	router.HandleFunc("/images", imagesHandle)
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 	http.Handle("/", router)
 	http.ListenAndServe(":"+port+"/", nil)
